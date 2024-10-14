edc2f5c98f55250817e9e74f7901c4aae48f2268
juangc.zip <jgarciac111@educastillalamancha.es>
2024-10-14 09:02:11 +0200

    update 2.0 | actualizacion pinocho

---
+++ unidad-1/proyecto-pinocho/proyecto-pinocho/Rio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_pinocho
+{
+  internal class Rio
+  {
+    public int[,] Contenido { get; private set; }
+    public int Filas { get; private set; }
+    public int Columnas { get; private set; }
+
+    public Rio(int inputFilas, int inputColumnas)
+    {
+      Filas = inputFilas;
+      Columnas = inputColumnas;
+
+      Contenido = new int[inputFilas, Columnas];
+      Random random = new Random();
+
+      for (int i = 0; i < Filas; i++)
+      {
+        for (int j = 0; j < Columnas; j++)
+        {
+          Contenido[i, j] = random.Next(0, 4);
+        }
+      }
+    }
+
+    public void Display(List<helperCharacter> inputChars)
+    {
+      Console.Clear();
+      for (int i = 0; i < Filas; i++)
+      {
+        for (int j = 0; j < Columnas; j++)
+        {
+          var tempChar = inputChars.Find(c => c.CurrentPosition.Item1 == i && c.CurrentPosition.Item2 == j);
+          if (tempChar != null)
+          {
+            Console.ForegroundColor = tempChar is Pinocho ? ConsoleColor.Blue : ConsoleColor.Green;
+
+            Console.Write(tempChar.Name[0] + " ");
+            Console.ResetColor();
+          }
+          else if (i == Filas - 1 && j == Columnas - 1)
+          {
+            Console.Write("» ");
+          }
+          else
+          {
+            Console.Write("~ ");
+          }
+        }
+
+        Console.WriteLine();
+      }
+    }
+  }
+}
---
+++ unidad-1/proyecto-pinocho/proyecto-pinocho/helperCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_pinocho
+{
+  abstract class helperCharacter
+  {
+    public string Name { get; protected set; }
+    public int Fish { get; protected set; }
+    public int Lives { get; protected set; }
+    public List<Tuple<int, int>> Path { get; protected set; }
+    public Tuple<int, int> CurrentPosition { get; protected set; }
+
+    public helperCharacter(string name)
+    {
+      Name = name;
+      Fish = 0;
+      Lives = 3;
+      Path = new List<Tuple<int, int>>();
+      CurrentPosition = new Tuple<int, int>(-1, -1);
+    }
+
+    public abstract void Move(Rio rio);
+
+    public bool IsAlive()
+    {
+      return Lives > 0 && Fish < 5 && Path.Count < 18;
+    }
+  }
+}
