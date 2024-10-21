5eba4f4a14dcc24d8c0cd37c5229eea16ab84f3b
juangc.zip <jgarciac111@educastillalamancha.es>
2024-10-21 18:33:09 +0200

    update 3.1 | ejemplo binding

---
+++ unidad-2/ejercicios-avanzados/ejercicioBinding1/ejercicioBinding1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ejercicioBinding1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      InitializeControls();
+    }
+
+    private void InitializeControls ()
+    {
+      lstBoxColorText.SelectedIndex = 0;
+      lstBoxFuentes.SelectedIndex = 0;
+
+      btnSizePequenio.Click += (s, e) => fontSlider.Value = 12;
+      btnSizeNormal.Click += (s, e) => fontSlider.Value = 16;
+      btnSizeGrande.Click += (s, e) => fontSlider.Value = 20;
+
+      txtBoxTamanioExacto.TextChanged += TxtBoxTamanioExacto_TextChanged;
+      fontSlider.ValueChanged += FontSlider_ValueChanged;
+    }
+
+    private void TxtBoxTamanioExacto_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      if (double.TryParse(txtBoxTamanioExacto.Text, out double size))
+      {
+        fontSlider.Value = size;
+      }
+    }
+
+    private void FontSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+      txtBoxTamanioExacto.Text = fontSlider.Value.ToString("0");
+    }
+  }
+}
