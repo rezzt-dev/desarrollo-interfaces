37ff5142e57f1d09e52c464aa9f22ffa9576af0e
juangc.zip <jgarciac111@educastillalamancha.es>
2024-10-28 10:36:49 +0100

    b

---
+++ unidad-2/ejercicios-avanzados/ejercicio-acceso-usuarios/ejercicio-acceso-usuarios/dto/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_acceso_usuarios.dto
+{
+  internal class Usuario
+  {
+    public String Login { get; set; }
+    public String Password { get; set; }
+
+    public Usuario(String login, String passw)
+    {
+      this.Login = login;
+      this.Password = passw;
+    }
+
+    public override String ToString()
+    {
+      return Login + " " + Password;
+    }
+  }
+}
---
+++ unidad-2/ejercicios-avanzados/ejercicio-acceso-usuarios/ejercicio-acceso-usuarios/vista/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ejercicio_acceso_usuarios.dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ejercicio_acceso_usuarios
+{
+  /// <summary>
+  /// Lógica de interacción para MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnAgregarCarrito_Click(object sender, RoutedEventArgs e)
+    {
+      Usuario usuario = new Usuario(tboxLogin.Text, tboxPassword.Password);
+      cboxListaUsuario.Items.Add(usuario.ToString());
+    }
+  }
+}
