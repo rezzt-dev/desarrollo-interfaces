d51efe1e9af6ad7372f8c124ed38315d75499c74
juangc.zip <jgarciac111@educastillalamancha.es>
2024-11-04 17:29:49 +0100

    update wpf | ludo mario bros

---
+++ unidad-2/LudoMarioBrosWPF/MarioBrosWPF/domain/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioBrosWPF.domain
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int posI { get; set; }
+        public int posJ { get; set; }
+        public int lifes { get; set; }
+        public int potions { get; set; }
+
+        public Player(string name) { 
+            this.name = name;
+            posI = 0;
+            posJ = 0;
+            lifes = 3;
+            potions = 0;
+        }
+    }
+}
