7ff079bb6e8449fc0d2e6ebe7cde73f77543c72e
rezzt.dev <rezzt.dev@proton.me>
2024-12-08 23:29:14 +0100

    di upate | proyecto tpv
    modelo de la bbdd e interfaz grafica

---
+++ di-proyecto-tpv/proyecto-tpv/proyecto-tpv/views/TPVWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proyecto_tpv.views
+{
+  /// <summary>
+  /// Interaction logic for TPVWindow.xaml
+  /// </summary>
+  public partial class TPVWindow : Window
+  {
+    public TPVWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void optExit_Click(object sender, RoutedEventArgs e)
+    {
+      Application.Current.Shutdown();
+    }
+
+    private void optCalendar_Click(object sender, RoutedEventArgs e)
+    {
+      CalendarWindow calendarWindow = new CalendarWindow();
+      calendarWindow.Show();
+      this.Close();
+    }
+
+    private void optSettings_Click(object sender, RoutedEventArgs e)
+    {
+      SettingsWindow settingsWindow = new SettingsWindow();
+      settingsWindow.Show();
+      this.Close();
+    }
+  }
+}
